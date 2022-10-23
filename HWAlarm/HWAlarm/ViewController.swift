6bc4880ac1bf4a59ba47c881f1f3b7cf15a78da0
DanyaDankou <93947618+DanyaDankou@users.noreply.github.com>
2022-10-23 23:53:08 +0300

    Add View Controller

--- HWAlarm/HWAlarm/ViewController.swift
+++ HWAlarm/HWAlarm/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var volumeLabel: UILabel!
+    @IBOutlet private weak var valueTextField: UITextField!
+    @IBOutlet private weak var progressView: UIProgressView!
+    @IBOutlet private weak var slider: UISlider!
+    @IBOutlet private weak var datePicker: UIDatePicker!
+    @IBOutlet private weak var button: UIButton!
+    @IBOutlet private weak var timeLabel: UILabel!
+    @IBOutlet private weak var `switch`: UISwitch!
+    @IBOutlet private weak var clearButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func textFieldAction(_ sender: UITextField) {
+    }
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        slider.minimumValue = 0
+        slider.maximumValue = 10
+        valueTextField.text = String (Int(slider.value.rounded(.down)))
+        progressView.progress = slider.value/10
+    }
+    
+    @IBAction func datePickerAction() {
+        
+    }
+    
+    @IBAction func buttonAction() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        let convertedDate = formatter.string(from: datePicker.date)
+        dismiss(animated: true, completion: nil)
+        timeLabel.text = String(convertedDate)
+    }
+    
+    @IBAction func switchAction() {
+        
+    }
+    
+    @IBAction func clearButtonAction() {
+        
+    }
+    
 
 }
 
